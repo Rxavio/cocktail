716023f57cd629a4990201cbb162588eecabe686
fxavio <xavier.rucahobatinya@amalitech.com>
2022-09-21 14:07:27 +0200

    set app basic requirements , structure, store and create slice

---
+++ src/components/SearchInput.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./SearchInput.css";
+
+const SearchInput = () => {
+ 
+  return (
+    <section className="section search">
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">Search Cocktail</label>
+
+        
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchInput;
